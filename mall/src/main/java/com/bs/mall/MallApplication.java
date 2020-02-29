68a91fc4ea065fee50fb13bd5619e88d160daffb
zxmaa <775396838@qq.com>
2020-02-29 14:46:50 +0800

    配置文件

--- mall/src/main/java/com/bs/mall/MallApplication.java
+++ mall/src/main/java/com/bs/mall/MallApplication.java
@@ -1,9 +1,14 @@
 package com.bs.mall;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 
 @SpringBootApplication
+@EnableTransactionManagement
+@MapperScan("com.bs.mall.dao")
 public class MallApplication {
 
     public static void main(String[] args) {
