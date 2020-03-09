5d2e31442061d29ec05bdd02ac06688045d7ca1e
xs <617269713@qq.com>
2020-03-09 11:41:45 +0800

    后台管理控制器

---
+++ mall/src/main/java/com/bs/mall/controller/admin/AdminLoginController.java
@@ -0,0 +1,15 @@
+package com.bs.mall.controller.admin;
+
+import com.bs.mall.controller.BaseController;
+import org.springframework.stereotype.Controller;
+
+/**
+ * author:xs
+ * date:2020/3/9 10:44
+ * description:后台登录控制器
+ */
+@Controller
+public class AdminLoginController extends BaseController {
+
+
+}
