b61007bf9cc81df49372bd2e6ec5a02a6fdeab47
xs <617269713@qq.com>
2020-03-18 17:01:08 +0800

    完善了各个service

---
+++ mall/src/main/java/com/bs/mall/service/ProductImageService.java
@@ -0,0 +1,17 @@
+package com.bs.mall.service;
+
+import com.bs.mall.entity.ProductImage;
+import com.bs.mall.util.PageUtil;
+
+import java.util.List;
+
+public interface ProductImageService {
+    boolean add(ProductImage productImage);
+    boolean addList(List<ProductImage> productImageList);
+    boolean update(ProductImage productImage);
+    boolean deleteList(Integer[] productImage_id_list);
+
+    List<ProductImage> getList(Integer product_id, Byte productImage_type, PageUtil pageUtil);
+    ProductImage get(Integer productImage_id);
+    Integer getTotal(Integer product_id, Byte productImage_type);
+}
---
+++ mall/src/main/java/com/bs/mall/service/PropertyService.java
@@ -0,0 +1,18 @@
+package com.bs.mall.service;
+
+import com.bs.mall.entity.Property;
+import com.bs.mall.util.PageUtil;
+
+import java.util.List;
+
+public interface PropertyService {
+    boolean add(Property property);
+
+    boolean addList(List<Property> propertyList);
+    boolean update(Property property);
+    boolean deleteList(Integer[] property_id_list);
+
+    List<Property> getList(Property property, PageUtil pageUtil);
+    Property get(Integer property_id);
+    Integer getTotal(Property property);
+}
