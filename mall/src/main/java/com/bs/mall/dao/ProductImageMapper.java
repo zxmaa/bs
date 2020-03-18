f72001989f968792951698617b27e8dc3953be48
xs <617269713@qq.com>
2020-03-18 17:16:48 +0800

    ProductImageMapper接口

--- mall/src/main/java/com/bs/mall/dao/ProductImageMapper.java
+++ mall/src/main/java/com/bs/mall/dao/ProductImageMapper.java
@@ -2,7 +2,11 @@ package com.bs.mall.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.bs.mall.entity.ProductImage;
+import com.bs.mall.util.PageUtil;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 
 /**
@@ -10,4 +14,13 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface ProductImageMapper extends BaseMapper<ProductImage> {
+    Integer insertOne(@Param("productImage") ProductImage productImage);
+    Integer insertList(@Param("productImage_list") List<ProductImage> productImageList);
+    Integer updateOne(@Param("productImage") ProductImage productImage);
+    Integer deleteList(@Param("productImage_id_list") Integer[] productImage_id_list);
+
+    List<ProductImage> select(@Param("product_id") Integer product_id, @Param("productImage_type") Byte productImage_type, @Param("pageUtil") PageUtil pageUtil);
+    ProductImage selectOne(@Param("productImage_id") Integer productImage_id);
+    Integer selectTotal(@Param("product_id") Integer product_id, @Param("productImage_type") Byte productImage_type);
+
 }
