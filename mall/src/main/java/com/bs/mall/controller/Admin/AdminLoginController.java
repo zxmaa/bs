cb3d03bb610b8c5480db70407a6f1016c23f9938
xs <617269713@qq.com>
2020-03-19 20:16:57 +0800

    AdminLoginController

---
+++ mall/src/main/java/com/bs/mall/controller/Admin/AdminLoginController.java
@@ -0,0 +1,44 @@
+package com.bs.mall.controller.Admin;
+
+import com.bs.mall.controller.BaseController;
+import com.bs.mall.service.IAdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+/**
+ * author:xs
+ * date:2020/3/19 14:21
+ * description:管理员登录controller
+ */
+@RestController
+@RequestMapping("/admin")
+public class AdminLoginController extends BaseController {
+    @Autowired
+    private IAdminService adminService;
+
+    //转到后台管理-登录页
+    @RequestMapping("/login")
+    public String goToPage(){
+        logger.info("转到后台管理-登录页");
+        return "admin/loginPage";
+    }
+
+    //登陆验证-ajax
+    @RequestMapping(value = "/login/doLogin",method = RequestMethod.POST,produces = "application/json;charset=utf-8")
+    public String checkLogin(HttpSession session, @RequestParam String username, @RequestParam String password){
+        return adminService.checkLogin(session, username, password);
+    }
+
+    //获取管理员头像路径-ajax
+    @RequestMapping(value = "/login/profile_picture",method = RequestMethod.GET,produces = "application/json;charset=utf-8")
+    public String getAdminProfilePicture(@RequestParam String username){
+        return adminService.getAdminProfilePicture(username);
+    }
+
+    //
+
+
+}
