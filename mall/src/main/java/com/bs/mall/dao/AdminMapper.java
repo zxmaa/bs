89ea46a2c3c043097fa882dcc2ee5fd5bb39c981
xs <617269713@qq.com>
2020-03-20 17:56:53 +0800

    更正了AdminMapper接口

--- mall/src/main/java/com/bs/mall/dao/AdminMapper.java
+++ mall/src/main/java/com/bs/mall/dao/AdminMapper.java
@@ -4,14 +4,17 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.bs.mall.dao.pojo.Admin;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 /**
  * 管理员Mapper接口
  */
+
+@Repository
 @Mapper
 public interface AdminMapper extends BaseMapper<Admin> {
     Admin selectByLogin(@Param("admin_name") String admin_name, @Param("admin_password") String admin_password);
 
-    Admin selectOne(@Param("admin_name") String admin_name, @Param("admin_id") Integer admin_id);
+    //Admin selectOne(@Param("admin_name") String admin_name, @Param("admin_id") Integer admin_id);
 
 }
