0a128b9df025b7aa9ea19cd1a43b685db1002e25
xs <617269713@qq.com>
2020-04-10 20:34:30 +0800

    新增了方法进mapper接口

--- mall/src/main/java/com/bs/mall/dao/ProductOrderItemMapper.java
+++ mall/src/main/java/com/bs/mall/dao/ProductOrderItemMapper.java
@@ -18,5 +18,6 @@ public interface ProductOrderItemMapper extends BaseMapper<ProductOrderItem> {
     List<com.bs.mall.entity.ProductOrderItem> select(@Param("pageUtil") PageUtil pageUtil);/*所有的*/
 
     List<com.bs.mall.entity.ProductOrderItem> selectByUserId(@Param("user_id") Integer user_id, @Param("pageUtil") PageUtil pageUtil);
+    List<com.bs.mall.entity.ProductOrderItem> selectByOrderId(@Param("order_id") Integer order_id, @Param("pageUtil") PageUtil pageUtil);
 
 }
--- mall/src/main/java/com/bs/mall/dao/ProductOrderMapper.java
+++ mall/src/main/java/com/bs/mall/dao/ProductOrderMapper.java
@@ -5,6 +5,8 @@ import com.bs.mall.dao.pojo.Product;
 import com.bs.mall.dao.pojo.ProductOrder;
 import com.bs.mall.dto.req.ForeOrderShowReqDto;
 import com.bs.mall.util.OrderGroup;
+import com.bs.mall.util.OrderUtil;
+import com.bs.mall.util.PageUtil;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
@@ -18,8 +20,12 @@ import java.util.List;
 @Repository
 @Mapper
 public interface ProductOrderMapper extends BaseMapper<ProductOrder> {
-    Integer selectTotal(@Param("productOrder") ProductOrder productOrder, @Param("productOrder_status_array") Byte[] productOrder_status_array);
+    Integer selectTotal(@Param("productOrder") com.bs.mall.entity.ProductOrder productOrder, @Param("productOrder_status_array") Byte[] productOrder_status_array);
     List<OrderGroup> getTotalByDate(@Param("beginDate") Date beginDate, @Param("endDate") Date endDate);
+    List<com.bs.mall.entity.ProductOrder> select(@Param("productOrder") com.bs.mall.entity.ProductOrder productOrder, @Param("product_order_status_array") Byte[] productOrder_status_array, @Param("orderUtil") OrderUtil orderUtil, @Param("pageUtil") PageUtil pageUtil);
+    com.bs.mall.entity.ProductOrder selectOne(@Param("productOrderId") Integer productOrderId);
+    Integer updateOne(@Param("productOrder") com.bs.mall.entity.ProductOrder productOrder);
+
 
     //=========================fore===========================
 
