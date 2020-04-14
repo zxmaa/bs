4be53e62f99538c82ecb6c1b6cf008c8541301c3
xs <617269713@qq.com>
2020-04-14 21:35:54 +0800

    启动类加了@ServletComponentScan

--- mall/src/main/java/com/bs/mall/MallApplication.java
+++ mall/src/main/java/com/bs/mall/MallApplication.java
@@ -3,11 +3,13 @@ package com.bs.mall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @SpringBootApplication
 @EnableTransactionManagement
+@ServletComponentScan
 @MapperScan("com.bs.mall.dao")
 public class MallApplication {
 
