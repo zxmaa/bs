a6f5b0a9ae99abc97fb2f2e4eda5fe9444aa6ad5
xs <617269713@qq.com>
2020-04-16 22:10:35 +0800

    更正了AdminHomeController

--- mall/src/main/java/com/bs/mall/controller/Admin/AdminHomeController.java
+++ mall/src/main/java/com/bs/mall/controller/Admin/AdminHomeController.java
@@ -3,10 +3,8 @@ package com.bs.mall.controller.Admin;
 import com.bs.mall.controller.BaseController;
 import com.bs.mall.service.admin.IAdminHomeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.text.ParseException;
@@ -19,23 +17,22 @@ import java.util.Map;
  * description:管理员主页控制器
  */
 
-@RestController
-@RequestMapping("/admin")
+@Controller
 public class AdminHomeController extends BaseController {
     @Autowired
     IAdminHomeService adminHomeService;
 
-    @RequestMapping(value = "/go", method = RequestMethod.GET)
+    @RequestMapping(value = "admin", method = RequestMethod.GET)
     public String goToPage(HttpSession session, Map<String, Object> map) throws ParseException {
         return adminHomeService.goToPage(session, map);
     }
 
-    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    @RequestMapping(value = "admin/home", method = RequestMethod.GET)
     public String goToPageByAjax(HttpSession session, Map<String, Object> map) throws ParseException {
         return adminHomeService.goToPageByAjax(session, map);
     }
-
-    @RequestMapping(value = "/home/charts", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
+    @ResponseBody
+    @RequestMapping(value = "admin/home/charts", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
     public String getChartDataByDate(@RequestParam(required = false) String beginDate, @RequestParam(required = false) String endDate) throws ParseException {
         return adminHomeService.getChartDataByDate(beginDate, endDate);
     }
